348ffb0c5f3181996fab1f7d1e3ce2a5bef118cf
Gustavo Holzmann <gustavobarretohv@live.com>
2023-12-05 19:41:23 -0300

    Added the Font Manager

---
+++ MGGame/Features/SplashScreen/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  MGGame
+//
+//  Created by Gustavo Holzmann on 04/12/23.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
